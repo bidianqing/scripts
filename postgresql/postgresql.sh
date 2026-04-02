4bbc8b480b6fd19b351fde4ecf93c9e2e1e01a6c
三大神捕之四 <bidianqing@qq.com>
2026-04-02 16:37:40 +0800

    Update postgresql.sh

--- postgresql/postgresql.sh
+++ postgresql/postgresql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install curl ca-certificates
+sudo apt install curl ca-certificates -y
 sudo install -d /usr/share/postgresql-common/pgdg
 sudo curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
 
